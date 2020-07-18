47144c911823448c1dbb409b875ce5bad34ec748
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-07-18 23:08:09 +0530

    importing electrone by npm

---
+++ main.js
@@ -0,0 +1,8 @@
+const electron = require('electrone')
+const url = require('url')
+const path = require('path')
+const { BrowserWindow } = require('electron')
+
+cosnt {app, BrowserWindow} = electron;
+
+let mainWindow;
