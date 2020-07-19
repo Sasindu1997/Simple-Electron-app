9376f232cacdfebcbc122f70b79994ca214109d6
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-07-19 13:35:19 +0530

    Creating dev tools

--- main.js
+++ main.js
@@ -43,6 +43,10 @@ function createAddWindow(){
          protocol: 'file:',
          slashes:true
      }))
+     //garbage collection handle
+     addWindow.on('close', function(){
+         addWindow = null;
+     });
 }
 
 //create menu template
@@ -69,3 +73,27 @@ const mainMenuTemplate = [
         ]
     }
 ];
+
+//if mac add empty object
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+//add developer tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu:[
+            {
+                label: 'Toggle DevTools',
+                accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
